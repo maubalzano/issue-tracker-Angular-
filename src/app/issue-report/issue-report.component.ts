66bc95b0d8f2ab8723dded2b97c06b32b91bd0d5
MAURIZIO <maurizio@iMac-di-Apple.wind3.hub>
2021-11-24 19:29:26 +0100

    add feature: report form validation

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IssuesService } from '../issues.service';
 
 @Component({
@@ -18,14 +18,18 @@ export class IssueReportComponent implements OnInit {
 
   ngOnInit(): void {
     this.issueForm = this.builder.group({
-      title: [''],
-      description: [''],
+      title: ['', Validators.required],
+      description: ['', Validators.required],
       priority: [''],
-      type: ['']
+      type: ['', Validators.required]
     });
   }
 
   addIssue(){
+    if (this.issueForm && this.issueForm.invalid){
+      this.issueForm.markAllAsTouched();
+      return
+    }
     this.issueService.createIssue(this.issueForm?.value);
     this.formClose.emit()
   }
